474dd14c840e679f0829822b97a024a97a3590c6
Michel Bitter <michel@michelbitter.nl>
2021-06-14 16:02:04 +0200

    Update linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,12 @@
 module.exports = {
-  env: {
-    es6: true,
-    mocha: true,
-    node: true,
-  },
-  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  root: true,
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
-  root: true,
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    'prettier/prettier': 2,
-    'sort-keys': 1,
   },
 }
